0cd28608db9f497790a344862ac2a8ba8c8d09cf
pts011080 <pts011080@hotmail.com>
2015-12-11 12:47:02 +0800

    Create new file for test
    create TestTableViewController for testing

---
+++ SunHorizontalScrollView/SunHorizontalScrollView/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  SunHorizontalScrollView
+//
+//  Created by LT on 10/12/15.
+//  Copyright © 2015 LT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+@end
